b14aa82ab689fc31856b9e818435d5d21a3e5638
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2024-03-25 16:31:10 +0100

    some minor work on the ui

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -1,18 +1,30 @@
 import { PrimaryButton, SecondaryButton } from "../Components/Buttons";
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
       <nav>
-        <div className="logo">
-          <p>RentALG</p>
-        </div>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="logo">
+            <p>RentALG</p>
+          </div>
+        </Link>
 
         <div className="right-side-nav">
           <SecondaryButton text="Login" />
           <PrimaryButton text="Sign up" />
         </div>
       </nav>
+      <div className="SearchSection">
+        <p>Where would you like to go?</p>
+        <input type="text" placeholder="Search destinations" />
+      </div>
+      <p
+        style={{ fontFamily: "Secular One", fontSize: 20, marginLeft: "50px" }}
+      >
+        Explore top listings
+      </p>
+      <main> 😀😀😀😀</main>
     </>
   );
 }
